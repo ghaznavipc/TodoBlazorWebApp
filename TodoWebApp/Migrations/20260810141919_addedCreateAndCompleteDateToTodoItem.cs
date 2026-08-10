00a25ebdf1fc244e52aa7dbfde66c85fccb698f5
ghaznavipc <ghaznavipc@gmail.com>
2026-08-10 17:31:08 +0300

    addedCreateAndCompleteDateToTodoItem

---
+++ TodoWebApp/Migrations/20260810141919_addedCreateAndCompleteDateToTodoItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedCreateAndCompleteDateToTodoItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CompleteDate",
+                table: "TodoItems",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDate",
+                table: "TodoItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompleteDate",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "TodoItems");
+        }
+    }
+}
--- TodoWebApp/Models/TodoItem.cs
+++ TodoWebApp/Models/TodoItem.cs
@@ -8,4 +8,6 @@ public class TodoItem
     [MaxLength(255)]
     public string? Title { get; set; } = string.Empty;
     public bool IsDone { get; set; }
+    public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+    public DateTime? CompleteDate { get; set; }
 }
